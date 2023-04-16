9763993dfd07221f8251f47246655aaa3df9bdb0
Sungwook Choi <sungwook.csw@gmail.com>
2023-04-16 23:56:39 +0900

    feat: add favicorn

--- docker-images/fastapi/main.py
+++ docker-images/fastapi/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
+favicon_path = "favicon.ico"
 
 
 @app.get("/")
-def hello_wolrd():
+async def hello_world():
     return {"framework": "fastapi", "message": "Hello World!"}
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
