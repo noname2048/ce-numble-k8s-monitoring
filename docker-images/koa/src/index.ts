76e36283619242908bf5ace747fb22737d657f42
Sungwook Choi <sungwook.csw@gmail.com>
2023-04-18 14:23:16 +0900

    feat: add favicon in koa

--- docker-images/koa/src/index.ts
+++ docker-images/koa/src/index.ts
@@ -1,11 +1,27 @@
+import fs from 'fs'
+
 import Koa, { Context } from 'koa';
+import Router from 'koa-router';
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx: Context) => {
+router.get('/', async (ctx: Context) => {
   ctx.body = { framework: 'koa', message: 'hello world!' };
 });
 
+router.options('/favicon.ico', async(ctx:Context) => {
+  ctx.status = 200;
+  ctx.set('Allow', 'GET, HEAD, OPTIONS')
+})
+
+router.get('/favicon.ico', async (ctx: Context) => {
+  ctx.set('Cache-Control', `public, max-age=${86400000 / 1000 | 0}`)
+  ctx.type= 'image/x-icon';
+  ctx.body = fs.readFileSync('./favicon.ico')
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 app.listen(4000, () => {
   console.log('Listening to port 4000');
 });
